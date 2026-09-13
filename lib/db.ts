028d70b6e2be825d6b8c5ca2fcd6b1031a10ae54
jay123244545 <mehtajay1232@gmail.com>
2026-09-14 00:55:05 +0530

    Set up database, prisma schema, and UI components

---
+++ lib/db.ts
@@ -0,0 +1,14 @@
+import { PrismaClient} from 'prisma/prisma-client'
+
+declare global {
+    var prisma: PrismaClient / undefined
+}
+
+const db = globalThis.prisma || new PrismaClient({
+    log: ['query', 'info', 'warn', 'error'],
+})
+
+if (process.env.NODE_ENV !=='development'){
+     globalThis.prisma = db
+}
+export default db 
